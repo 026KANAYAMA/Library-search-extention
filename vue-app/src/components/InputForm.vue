9024748a4c416af6ef9765dc850001cd22c09b01
026KANAYAMA <103913975+026KANAYAMA@users.noreply.github.com>
2023-02-21 20:30:51 +0900

    Make input form

---
+++ vue-app/src/components/InputForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input-form">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="URL">
+    <button @click="PrintURL">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputForm',
+  data(){
+    return {
+      msg : '本のURLを入力してください',
+      URL : ''
+    }
+  },
+  methods : {
+    PrintURL (){
+      this.msg = 'あなたが入力したURLは「' + this.URL + '」です'
+    }
+  }
+  
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
