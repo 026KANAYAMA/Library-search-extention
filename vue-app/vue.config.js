807af087240868912d3666b518ebf19b1539e1dd
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-03-03 16:11:57 +0900

    VueCLIのシミュレータのパスとGitHubPagesのパスを同期

--- vue-app/vue.config.js
+++ vue-app/vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir:"../docs"
+  outputDir:"../docs",
+  publicPath: '/Library-search-extention'
 })
